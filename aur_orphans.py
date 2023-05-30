7e14c42ccf938443b876ab0837fca75118e3c8e5
jmcb <joelsgp@protonmail.com>
2023-05-30 18:21:41 +0100

    Implement finding orphaned packages

--- aur_orphans.py
+++ aur_orphans.py
@@ -30,13 +30,28 @@ def list_out_of_date(package_names: PackageNames) -> PackageNames:
         if out_of_date is not None:
             timestamp = datetime.utcfromtimestamp(out_of_date)
             time_formatted = timestamp.isoformat()
-            print(f"Out of date: {name}, {time_formatted}")
+            print(f"Out of date ({time_formatted}): {name}")
         else:
             if PRINT_OK:
                 print(f"OK: {name}")
 
     hit_packages = frozenset(hit_packages)
     return hit_packages
+
+
+def list_orphaned(package_names: PackageNames) -> PackageNames:
+    print("Getting orphaned packages from AUR rpc")
+    aur = AurRpc()
+    orphaned_packages = aur.search(by="maintainer")
+
+    hit_packages = frozenset(p["Name"] for p in orphaned_packages)
+    orphaned_packages = package_names.intersection(hit_packages)
+
+    for package in orphaned_packages:
+        print(f"Orphaned: {package}")
+
+    return hit_packages
+
 
 
 def main():
@@ -46,9 +61,8 @@ def main():
     package_names = frozenset(stdout.splitlines())
     print(f"Arch wiki lists {len(package_names)} packages")
 
-    list_out_of_date(package_names)
-
-    # print("Getting orphaned packages from AUR rpc")
+    # list_out_of_date(package_names)
+    list_orphaned(package_names)
 
 
 if __name__ == "__main__":
