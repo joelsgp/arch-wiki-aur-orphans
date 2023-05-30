b266ed7c84a600fff88129b58d4da0ea726208e3
jmcb <joelsgp@protonmail.com>
2023-05-30 16:24:49 +0100

    Initial commit

---
+++ list-wiki.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eux
+grep --recursive --only-matching --no-filename --perl-regexp \
+    --regexp='(?<=https://aur.archlinux.org/packages/)[a-z0-9@\._+\-]+(?=/)' \
+    '/usr/share/doc/arch-wiki/html/' \
+    | sort | uniq -u
